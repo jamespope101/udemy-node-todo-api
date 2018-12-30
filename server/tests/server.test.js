952ea974bf042ef906e0f23fc6c41cce48375184
James Pope <jamespope101@gmail.com>
2018-12-30 17:36:31 +0000

    test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -68,4 +71,33 @@ describe('GET /todos', () => {
             })
             .end(done);
     });
-})
+});
+
+describe('GET /todo', () => {
+    it('should return todo', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+    
+    it('should throw 404 if todo not found', (done) => {
+        var unknownId = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${unknownId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should throw 400 if invalid id', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(400)
+            .end(done);
+    });
+
+
+});
